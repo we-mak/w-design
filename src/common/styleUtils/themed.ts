73cf6ed72cb7abd8f0592b331c4a6b12795d336a
Son_dang <son.dang@doda100.com>
2019-01-05 16:34:15 +0700

    finish button without get from theme

--- src/common/styleUtils/themed.ts
+++ src/common/styleUtils/themed.ts
@@ -3,6 +3,7 @@ import { setColors } from "./utils";
 // default theme colors
 const {
   N1,
+  N2,
   N3,
   N4,
   N90,
@@ -35,26 +36,26 @@ const {
 
 // Background colors
 export const bg = setColors([N3, B50, O30, R30, G30, WHITE, WHITE, T30]);
-export const bgHover = setColors([N4, B40, O20, R20, G20, N3, WHITE, T20]);
-export const bgActiveFocus = setColors([B5, B60, O40, R40, G40, B5, B5, T40]);
+export const bgHover = setColors([N4, B40, O20, R20, G20, N3, N2, T20]);
+export const bgActive = setColors([B5, B60, O40, R40, G40, B5, B5, T40]);
 // Border colors
 export const bordr = setColors([N3, B60, O40, R40, G40, "", "", T40]);
 export const bordrHover = setColors([N4, B50, O30, R30, G30, "", "", T30]);
-export const bordrActiveFocus = setColors([
-  B5,
-  B70,
-  O50,
-  R50,
-  G50,
-  "",
-  "",
-  T50
-]);
+export const bordrActive = setColors([B5, B70, O50, R50, G50, "", "", T50]);
 
 // Text
 export const text = setColors([N90, N1, N1, N1, N1, B50, N90, N1]);
 // outline
-export const boxShadow = setColors(["transparent", B5, O10, R5, G5, B50]);
+export const boxShadow = setColors([
+  "transparent",
+  B5,
+  O10,
+  R5,
+  G5,
+  B50,
+  B50,
+  T50
+]);
 
 // Button, input padding style
 export const padding = {
--- src/components/elements/Button/getButtonStyle.ts
+++ src/components/elements/Button/getButtonStyle.ts
@@ -13,12 +13,13 @@ import {
   padding,
   height,
   bg,
-  bgActiveFocus,
+  bgActive,
   bgHover,
   bordr,
-  bordrActiveFocus,
+  bordrActive,
   bordrHover,
-  text
+  text,
+  boxShadow
 } from "../../../common/styleUtils/themed";
 
 /** Apply style to each appearance when isLoading = true */
@@ -39,6 +40,58 @@ const getLoadingState = (props: ButtonProps) => {
   return;
 };
 
+const getSelected = (props: any) => {
+  const { appearance, isSelected, theme } = props;
+
+  if (isSelected) {
+    if (appearance === "danger")
+      return css`
+        border-color: ${getColor(colors, "R40", theme)};
+        background: ${getColor(colors, "R30", theme)};
+        &:hover,
+        &:focus,
+        &:active {
+          box-shadow: none;
+          border-color: ${getColor(colors, "R40", theme)};
+          background: ${getColor(colors, "R30", theme)};
+        }
+      `;
+
+    if (appearance === "warning")
+      return css`
+        border-color: ${getColor(colors, "O50", theme)};
+        background: ${getColor(colors, "O40", theme)};
+        &:hover,
+        &:focus,
+        &:active {
+          box-shadow: none;
+          border-color: ${getColor(colors, "O50", theme)};
+          background: ${getColor(colors, "O40", theme)};
+        }
+      `;
+
+    return css`
+      background: ${getColor(colors, "B70", theme)};
+      border-color: ${getColor(colors, "B50", theme)};
+      color: ${getColor(colors, "N1", theme)};
+      &:hover,
+      &:focus,
+      &:active {
+        background: ${getColor(colors, "B70", theme)};
+        border-color: ${getColor(colors, "B50", theme)};
+        color: ${getColor(colors, "N1", theme)};
+        outline: none;
+        box-shadow: none;
+      }
+    `;
+  }
+  return;
+};
+
+/**
+ * @function getButtonStyle
+ * @param props Props from component
+ */
 export function getButtonStyle(props: any) {
   const { appearance, size, fluid, theme } = props;
 
@@ -51,9 +104,14 @@ export function getButtonStyle(props: any) {
   if (fluid) width = "100%";
 
   /** Button appearance style*/
-  const backgroundStyle = getAppearanceProps(appearance, bg);
+  const bgStyle = getAppearanceProps(appearance, bg);
+  const bgHoverStyle = getAppearanceProps(appearance, bgHover);
+  const bgActiveStyle = getAppearanceProps(appearance, bgActive);
   const borderStyle = getAppearanceProps(appearance, bordr);
+  const borderHoverStyle = getAppearanceProps(appearance, bordrHover);
+  const borderActiveStyle = getAppearanceProps(appearance, bordrActive);
   const colorStyle = getAppearanceProps(appearance, text);
+  const boxShadowStyle = getAppearanceProps(appearance, boxShadow);
 
   // Apply loading style
   const loadingStyle = getLoadingState(props);
@@ -77,9 +135,30 @@ export function getButtonStyle(props: any) {
     padding: ${paddingStyle};
     height: ${heightStyle};
     font-size: ${fontSizeStyle};
-    background: ${backgroundStyle};
+    background: ${bgStyle};
     border-color: ${borderStyle};
     color: ${colorStyle};
+    box-shadow: 0 0 0 0.1rem var(--box-shadow-color);
+
+    &:hover {
+      background: ${bgHoverStyle};
+      border-color: ${borderHoverStyle};
+    }
+
+    &:active {
+      background: ${bgActiveStyle};
+      border-color: ${borderActiveStyle};
+      --box-shadow-color: ${boxShadowStyle};
+    }
+
+    &:disabled,
+    &[disabled] {
+      cursor: not-allowed;
+      color: ${getColor(colors, "N7", theme)} !important;
+      background: ${getColor(colors, "N4", theme)} !important;
+      border: none;
+    }
+    ${getSelected(props)};
 
     ${loadingStyle};
     ${borderRadius};

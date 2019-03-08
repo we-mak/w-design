af013c5be46e07c6bf1266c4c016c6208583f4e9
Son_dang <son.dang@doda100.com>
2019-03-08 21:57:10 +0700

    upgrade package

--- example/src/App.js
+++ example/src/App.js
@@ -1,21 +1,12 @@
 import React, { Component } from "react";
-import { Provider, Container, Button } from "w-design";
+import { Provider, Container } from "w-design";
 
 class App extends Component {
   render() {
     return (
       <>
         <Provider>
-          <Container p={2}>
-            <Button>Button</Button>{" "}
-            {/* <Button appearance="primary">Button</Button>{" "}
-            <Button appearance="warning">Button</Button>{" "}
-            <Button appearance="danger">Button</Button>{" "}
-            <Button appearance="success">Button</Button>{" "}
-            <Button appearance="link">Button</Button>{" "}
-            <Button appearance="subtle">Button</Button>{" "}
-            <Button appearance="help">Button</Button> */}
-          </Container>
+          <Container p={2}>Content</Container>
         </Provider>
       </>
     );

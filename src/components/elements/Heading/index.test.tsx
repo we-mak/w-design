c7c474dfc650f0ec09f95e8dc2f29928d7fd41fe
Son_dang <son.dang@doda100.com>
2019-03-09 08:45:42 +0700

    update test

--- src/components/elements/Heading/index.test.tsx
+++ src/components/elements/Heading/index.test.tsx
@@ -2,19 +2,20 @@ import * as React from "react";
 import { render } from "react-testing-library";
 import "jest-styled-components";
 import Heading from "./index";
+import Provider from "../../layout/Provider";
 
-describe("<Grid/>", () => {
+describe("<Heading/>", () => {
   // Render test
   it("should renders correctly", () => {
     const { container } = render(
-      <>
+      <Provider>
         <Heading.H1 />
         <Heading.H2 />
         <Heading.H3 />
         <Heading.H4 />
         <Heading.H5 />
         <Heading.H6 />
-      </>
+      </Provider>
     );
     expect(container).toMatchSnapshot();
   });

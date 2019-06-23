03bc9c1687e7ef677d8ca588a2426cfdf4dbb75f
Son_dang <son.dang@doda100.com>
2019-06-23 07:03:59 +0700

    remove react test renderer

--- src/components/layout/DisplayElement/index.test.tsx
+++ src/components/layout/DisplayElement/index.test.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-// import * as renderer from "react-test-renderer";
 import { render } from "@testing-library/react";
 import "jest-styled-components";
 import DisplayElement from "./index";

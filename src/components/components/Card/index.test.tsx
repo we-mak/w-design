d59264ef51cc1e9aca9bc7876681dfd556a98f7c
Son_dang <son.dang@doda100.com>
2019-06-24 09:38:55 +0700

    add card test

---
+++ src/components/components/Card/index.test.tsx
@@ -0,0 +1,34 @@
+import * as React from "react";
+import { cleanup, render } from "@testing-library/react";
+import "jest-styled-components";
+import Card, { CardMedia, CardHeader, CardContent, CardFooter } from "./index";
+
+import Provider from "../../layout/Provider";
+
+afterEach(cleanup);
+
+describe("<Button/>", () => {
+  it("should render with different appearance style", () => {
+    const { container } = render(
+      <Provider>
+        <Card raised>
+          <CardHeader title="Test title" description="Lorem ipsum">
+            Header
+          </CardHeader>
+
+          <CardMedia
+            imageUrl="https://dak95nwic4sny.cloudfront.net/73/cheetah-safari-41193237-1554105425-ImageGalleryLightboxLarge.jpg"
+            alt="test"
+          />
+          <CardContent>
+            A safari is an overland journey, usually a trip by tourists in Africa. In the past, the
+            trip was often a big-game hunt, but today, safaris are often to observe
+          </CardContent>
+
+          <CardFooter>Footer</CardFooter>
+        </Card>
+      </Provider>
+    );
+    expect(container).toMatchSnapshot();
+  });
+});

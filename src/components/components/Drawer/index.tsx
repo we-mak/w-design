a6e955affd3bb2f45b144aa3eb6810b421943eae
Son_dang <son.dang@doda100.com>
2019-08-31 18:03:32 +0700

    finish Drawer

--- src/components/components/Drawer/index.tsx
+++ src/components/components/Drawer/index.tsx
@@ -1,28 +1,25 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { disableBodyScroll, clearAllBodyScrollLocks, BodyScrollOptions } from "body-scroll-lock";
-import Portal from "../../elements/Portal";
-import { Overlay, DrawerPanel } from "./Styled";
+import { DrawerContainer, Overlay, DrawerPanel } from "./Styled";
 import { DrawerProps } from "./types";
 
 const options: BodyScrollOptions = {
   reserveScrollBarGap: true
 };
-// Main root layout selecting
 const documentBody: any = document.body;
 
 const Drawer = ({ children, isOpen, onClose }: DrawerProps) => {
-  useEffect(() => {
-    if (isOpen) {
-      disableBodyScroll(documentBody, options);
-    }
-    return () => clearAllBodyScrollLocks();
-  }, []);
+  if (isOpen) {
+    disableBodyScroll(documentBody, options);
+  } else {
+    clearAllBodyScrollLocks();
+  }
 
   return (
-    <Portal>
+    <DrawerContainer>
+      <Overlay isOpen={isOpen} onClick={onClose} />
       <DrawerPanel isOpen={isOpen}>{children}</DrawerPanel>
-      {isOpen && <Overlay onClick={onClose} />}
-    </Portal>
+    </DrawerContainer>
   );
 };
 

aafda5c7a17cc83dc29be4384a96c25777ad43b2
Son_dang <son.dang@doda100.com>
2019-12-09 20:32:55 +0700

    remove peer deps

--- packages/form/src/index.ts
+++ packages/form/src/index.ts
@@ -1,10 +1,10 @@
 export { default as Checkbox } from "./Checkbox";
-export { default as FormGroup } from "./FormGroup";
-export { default as Label } from "./Label";
-export { default as InputField } from "./InputField";
-export { default as InputForm } from "./InputForm";
-export { default as RadioGroup } from "./Radio";
-export { default as TextArea } from "./TextArea";
-// hooks
-export { default as useField } from "./hooks/useField";
-export { default as useForm } from "./hooks/useForm";
+// export { default as FormGroup } from "./FormGroup";
+// export { default as Label } from "./Label";
+// export { default as InputField } from "./InputField";
+// export { default as InputForm } from "./InputForm";
+// export { default as RadioGroup } from "./Radio";
+// export { default as TextArea } from "./TextArea";
+// // hooks
+// export { default as useField } from "./hooks/useField";
+// export { default as useForm } from "./hooks/useForm";
--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,3 @@
-import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import typescript from "rollup-plugin-typescript2";
 import commonjs from "rollup-plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
@@ -44,7 +43,6 @@ export default {
     }
   ],
   plugins: [
-    peerDepsExternal(),
     url(),
     svgr(),
     postcss({
